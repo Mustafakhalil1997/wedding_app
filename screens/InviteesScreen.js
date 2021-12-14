c47ca3847f21a511d68ef54360cf78acc8efafe8
Mustafa Khalil <mustafakh1997@gmail.com>
2021-12-14 18:29:50 +0200

    added testing for realtime data fetch
    fetching data in realtime inside inviteeScreen, should then fetch it and add it to redux store

--- screens/InviteesScreen.js
+++ screens/InviteesScreen.js
@@ -1,13 +1,48 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import InviteeList from "../components/InviteeList";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAioK_0pdMEniIqmQ97K2HkmaaYWoSo8Wk",
+  authDomain: "weddingproject2-ce55f.firebaseapp.com",
+  databaseURL: "https://weddingproject2-ce55f-default-rtdb.firebaseio.com",
+  projectId: "weddingproject2-ce55f",
+  storageBucket: "weddingproject2-ce55f.appspot.com",
+  messagingSenderId: "917688119636",
+  appId: "1:917688119636:web:033e8eecfbb0b0699018c1",
+  measurementId: "G-N61VX498CT",
+};
+
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+
+const db = getDatabase();
+const list = ref(db, "invitees");
 
 const InviteesScreen = ({ navigation }) => {
   const dummy_list = useSelector((state) => state.inviteeList.inviteeList);
 
   const [inviteeList, setInviteeList] = useState(dummy_list);
   const [searchList, setSearchList] = useState(dummy_list);
+
+  setInterval(
+    onValue(list, (snapshot) => {
+      console.log("onValue is here");
+      const data = snapshot.val();
+      console.log("data ", data);
+      console.log("dataaa", data[Object.keys(data)[1]]);
+      console.log(Object.keys(data).length);
+      // setSearchList(data);
+    }),
+    6000
+  );
 
   const textChangeHandler = (value) => {
     console.log("value ", value);
