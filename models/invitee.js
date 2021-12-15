67872d017be134a598b7bffbed0cec2a980e4fb2
Mustafa Khalil <mustafakh1997@gmail.com>
2021-12-15 18:53:08 +0200

    added an invitee class
    after getting the list of invitees from firebase I'm populating each invitee to an invitee class and then store them an array inside redux store

---
+++ models/invitee.js
@@ -0,0 +1,10 @@
+class Invitee {
+  constructor(id, name, isPriority, checkIn) {
+    this.id = id;
+    this.name = name;
+    this.isPriority = isPriority;
+    this.checkIn = checkIn;
+  }
+}
+
+export default Invitee;
